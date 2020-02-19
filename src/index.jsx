b85782e4f54d43b087d1edf78cca50143f386882
Fabian Rosinger <fabianrosinger@gmail.com>
2020-02-19 22:55:34 +0100

    Add app.scss

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-import 'src/themes/bootstrap.scss';
+import 'app.scss';
 
 ReactDOM.render(<App />, document.getElementById('app'));
